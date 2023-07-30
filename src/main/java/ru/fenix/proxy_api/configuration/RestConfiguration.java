fad0163753c142508f94d480a94749c9b7ffca08
Андрей Полетаев <a.poletaev@automacon.ru>
2023-07-30 14:03:51 +0300

    ++

---
+++ src/main/java/ru/fenix/proxy_api/configuration/RestConfiguration.java
@@ -0,0 +1,18 @@
+package ru.fenix.proxy_api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RestConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+                .favorParameter(false)
+                .ignoreAcceptHeader(true)
+                .defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
